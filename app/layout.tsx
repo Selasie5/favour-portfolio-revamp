281fedf8d5f6adc9d41d2d6fff6b0c0437b6014e
Selasie5 <selasisepenu5@gmail.com>
2025-02-07 12:10:22 +0000

    Implement analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react"
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./Navbar"
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Analytics/>
         <CustomCursor/>
         <Navbar/>
         {children}
